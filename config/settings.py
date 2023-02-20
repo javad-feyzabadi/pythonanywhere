3313483d1c8524c4694592861af6fef2dccf52b2
javad <javad.feyzabadi@yahoo.com>
2023-02-21 02:03:38 +0330

    fandogh

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*+] [AMPTRN [TYPEPTRN]]  list operator cl']
 
 
 # Application definition
@@ -132,10 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static_root/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = '/media/'
